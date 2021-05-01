88e3fa635e837e7cd136f1a2d0f6065cee5e26bb
SeatacAlert <76701215+SeatacAlert@users.noreply.github.com>
2021-04-30 22:19:36 -0700

    Update 437.py

--- 437.py
+++ 437.py
@@ -1,3 +1,6 @@
+# 假设已经保存i-j的和，那么需要添加第j+1个元素的时候，每个元素都要加j+1，麻烦
+# 所以假定所有的元素都减去了1--j+1
+# 因此实际上这里保存的是-(1--i)的和
 class Solution:
     def pathSum(self, root: TreeNode, targetSum: int) -> int:
         cnt = {}
