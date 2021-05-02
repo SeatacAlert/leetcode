ddc9dc48b0553337657c22dc72394e04835dd2a1
SeatacAlert <76701215+SeatacAlert@users.noreply.github.com>
2021-05-02 14:39:31 -0700

    Create 1850.cpp

---
+++ 1850.cpp
@@ -0,0 +1,22 @@
+//重点是，next_permutation 牛
+class Solution {
+public:
+    int getMinSwaps(string num, int k) {
+        string target(num);
+        for (int i = 0; i < k; ++i)
+            next_permutation(target.begin(), target.end());
+        
+        int ans = 0, n = num.size();
+        for (int i = 0; i < n; ++i) {
+            if (num[i] != target[i]) {
+                int j = i + 1;
+                while (num[j] != target[i])
+                    j++;
+                for (; j > i; --j)
+                    swap(num[j], num[j - 1]), ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
