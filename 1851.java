3742827781691269562538995da746691ca4ca8d
SeatacAlert <76701215+SeatacAlert@users.noreply.github.com>
2021-05-02 15:16:57 -0700

    Update 1851.java

--- 1851.java
+++ 1851.java
@@ -2,6 +2,7 @@
 //思路与我不一样，他先将所有要查询的点query放到TreeMap里，然后用keySet得到TreeSet tset，是一个排了序的query点集合
 //所有的interval按长度从小到大排序，然后对于每个区间intv，在tset中找到在这个区间中的点，这些点的最佳长度就更新了，更新以后从tset中移除掉
 //这里完美的利用了TreeSet的方法 tset.subSet(start, end) 以及 集合的 tset.add(ele) tset.removeAll(tset2)
+//这个方法的缺点是复杂度高，因为对interval和queries都进行了排序。
 class Solution {
    public int[] minInterval(int[][] intervals, int[] queries) {
 
